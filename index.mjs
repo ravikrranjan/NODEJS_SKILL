9ff1b3a8715979dfc7de62e048accf0c1048f1e5
Ravi Ranjan <raviranjan1600@gmail.com>
2022-07-03 20:08:17 +0530

    get method and pathname

--- index.mjs
+++ index.mjs
@@ -1,23 +1,33 @@
 /**
  * Primary file for API
+ *
+ * @auth Ravi Ranjan
+ * @Date July-22
  */
 
 //Dependencies
 
 import http from "http";
+import url from "url";
 
 // The server should res;ond to all requests with a string
 
 const server = http.createServer((request, response) => {
 	// Get the URL and parse it
 
-	// Get the path
+	const parsedUrl = url.parse(request.url, true);
 
+	// Get the path
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+	const method = request.method.toLowerCase();
 	// Send the response
 
 	// Log the request path
 
 	response.end("Hello World\n");
+
+	console.log("Request received on path: ", trimmedPath, " with ", method);
 });
 
 // start the server , and have it listen on port 3737
